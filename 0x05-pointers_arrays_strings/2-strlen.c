9271892070e2d9530927074bca086e11b540b70a
Christiancode-beep <christianobinna71@gmail.com>
2022-03-21 06:32:23 -0700

    Add 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * _strlen - returns th elength of a string
  * @s: string to evaluate
  * Return: the lenght of the string
  */
-int _strlen_(char *s)
+
+int _strlen(char *s)
 {
 
 int i;
