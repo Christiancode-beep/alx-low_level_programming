9d498574aa191469d21c47b5429e77c365e4f79b
Christiancode-beep <christianobinna71@gmail.com>
2022-03-21 12:53:21 -0700

    Add 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,14 @@ void print_rev(char *s)
 
 {
 	int i, j, len;
-	
 	i = 0;
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 	
 len = i;
-		for(j = len - 1; j >=0; j--)
+		for (j = len - 1; j >= 0; j--)
 		{
 			_putchar(s[j]);
 		}
