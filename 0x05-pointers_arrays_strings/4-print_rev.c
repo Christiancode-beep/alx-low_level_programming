7412ca41ec218f3b30a7d42f0e3b2ec711c8b875
Christiancode-beep <christianobinna71@gmail.com>
2022-03-21 12:37:46 -0700

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse direction/order
+ * @s:  string to be printed
+ */
+
+void print_rev(char *s)
+
+{
+	int i, j, k;
+	
+	i = 0;
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	
+	j = len
+		For(j = len - 1; j >=0; j--)
+		{
+			_putchar(s[j]);
+		}
+_putchar('\n');
+
+}
